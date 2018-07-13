54044190f78b26f2c2f01c88cb75ed5b6198c502
squamuglia <maxsmouha@gmail.com>
2018-07-13 11:35:40 -0400

    it works

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const ac = new(window.AudioContext ||
     window.msAudioContext);
 
     //CONSTANTS VARIABLES
-const url = 'http://localhost:3000'
 const frequencyList = {"C": 32.70, "C#":34.65, "D":36.71, "Eb":38.89,	"E":41.20, "F":43.65, "F#":46.25,	"G":49.00, "G#":51.91,	"A":55.00,	"Bb":58.27,	"B":61.74} // frequency - key hash
 const keyValues = { "A":"C", "W":"C#", "S":"D", "E":"Eb", "D":"E", "F":"F" , "R":"F#", "G":"G" , "T":"G#", "H":"A" , "Y":"Bb", "J":"B" } //key - note hash for keypress
 
@@ -256,23 +255,43 @@ document.getElementById('url_song_name').value = currentSong.name
 
 const copyBtn = document.getElementById('copy_url')
 
-copyBtn.addEventListener('click',
-
-)
-
-function copy() {
-    debugger;
-    // /* Get the text field */
-    // let copy = window.location.href;
-  
-    // /* Select the text field */
-    // copy.select();
-  
-    // /* Copy the text inside the text field */
-    // document.execCommand("copy");
+copyBtn.addEventListener('click', e => {
     urlToSong()
-    window.clipboardData.setData("Text", songLocation);
+    copyToClipboard(songLocation)
     songInputName.value = "Your Song Was Copied. Share With Your Friends!"
     setTimeout(()=> songInputName.value = "Record Another Song!", 5000)
+})
 
+  function isOS() {
+    return navigator.userAgent.match(/ipad|iphone/i);
   }
+  
+  function copyToClipboard (msg) {
+    try {
+      const text = document.createElement('textarea')
+      text.value = msg
+      text.style.cssText = 'position: absolute; left: -9999px;'
+      document.body.appendChild(text)
+  
+      if (isOS()) {
+        const range = document.createRange()
+        range.selectNodeContents(text)
+  
+        const selection = window.getSelection()
+  
+        selection.removeAllRanges()
+        selection.addRange(range)
+        text.setSelectionRange(0, 999999);
+      } else {
+        text.select();
+      }
+  
+      document.execCommand('copy')
+      text.blur()
+      document.body.removeChild(text)
+    } catch (err) {
+      window.prompt('Press Cmd+C or Ctrl+C to copy to clipboard.', msg)
+    }
+  }
+  
+  
